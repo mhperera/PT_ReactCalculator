c23626c3a777b3162b35c69a831be3f965a72023
zmhp <manashahp1995@gmail.com>
2023-10-06 14:19:06 +0530

    Add Calculator Styles

---
+++ src/Calculator.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const Calculator = () => {
+
+    return (
+        <div className='calculator-grid'>
+
+            <div className="output">
+                <div className="previous-operand"></div>
+                <div className="current-operand"></div>
+            </div>
+
+            <button className="span-two action">AC</button>
+            <button className="action">DEL</button>
+            <button className="action">/</button>
+
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button className="action">*</button>
+
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button className="action">+</button>
+
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button className="action">-</button>
+
+            <button>.</button>
+            <button>0</button>
+            <button className="span-two">=</button>
+
+        </div>
+    )
+}
+
+export default Calculator
